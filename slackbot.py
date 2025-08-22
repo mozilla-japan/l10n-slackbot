950d4a1e8f90ae87e960c688b1fe053ce66b3c07
hATrayflood <h.rayflood@gmail.com>
2025-08-22 19:48:32 +0900

    migrate slackbot from lot

---
+++ slackbot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import datetime
+import os
+import sys
+import traceback
+
+# pip install
+import requests
+import pyquery.pyquery
+from compare_locales.paths import TOMLParser
+from compare_locales.compare import compareProjects
+
+PONTOON_URL = "https://pontoon.mozilla.org/ja/firefox/"
+
+
+def calendarCheck():
+    d = pyquery.pyquery.PyQuery(url=PONTOON_URL)
+    deadline = datetime.datetime.strptime(d("#heading li.deadline time").text(), "%b %d, %Y")
+    remain = deadline.date() - datetime.datetime.now(datetime.timezone.utc).date()
+    return "次回締切まであと *%d 日* (%s)\n" % (remain.days, deadline.date())
+
+
+def compare(browser_toml, l10n_base_dir):
+    config_env = {"l10n_base": l10n_base_dir}
+    configs = [TOMLParser().parse(browser_toml, env=config_env)]
+    return compareProjects(configs, ["ja"], l10n_base_dir).toJSON()
+
+def l10nStatus(browser_toml, mj_git_repo, l10n_central):
+    missing         = compare(browser_toml,  mj_git_repo)["summary"]["ja"].get("missing", 0)
+    missing_central = compare(browser_toml, l10n_central)["summary"]["ja"].get("missing", 0)
+
+    if missing:
+        message = "firefoxの翻訳状況: *%d missing*" % (missing)
+    else :
+        message = "firefoxの翻訳状況: Translated"
+    if missing > missing_central:
+        message += "\n_l10n-centralが先行:_ %d Translated" % (missing - missing_central)
+    return message
+
+
+def postSlack(token, text):
+    data = {"token":token, "channel":"#l10n", "text":text}
+    requests.post("https://slack.com/api/chat.postMessage", data=data)
+
+
+if __name__ == "__main__":
+    token = os.environ.get("L10NBOT_TOKEN")
+    try:
+        (browser_toml, mj_git_repo, l10n_central) = sys.argv[1:]
+        text = calendarCheck() + l10nStatus(browser_toml, mj_git_repo, l10n_central)
+    except:
+        text = "```%s```" % traceback.format_exc()
+    finally:
+        if token:
+            postSlack(token, text)
+        else:
+            print(text)
---
+++ slackbot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CURDIR=$(cd $(dirname ${0}) && pwd)
+GECKO_STRINGS=${CURDIR}/firefox-l10n-source
+L10N_CENTRAL=${CURDIR}/firefox-l10n
+MJ_GIT_REPO=${CURDIR}/gecko-l10n
+
+if [ ! -f ${GECKO_STRINGS}/.git/config ] ; then
+    mkdir -p ${GECKO_STRINGS}
+    git clone https://github.com/mozilla-l10n/firefox-l10n-source.git ${GECKO_STRINGS}
+fi
+if [ ! -f ${L10N_CENTRAL}/.git/config ] ; then
+    mkdir -p ${L10N_CENTRAL}
+    git clone https://github.com/mozilla-l10n/firefox-l10n.git ${L10N_CENTRAL}
+fi
+if [ ! -f ${MJ_GIT_REPO}/.git/config ] ; then
+    mkdir -p ${MJ_GIT_REPO}
+    git clone https://github.com/mozilla-japan/gecko-l10n.git ${MJ_GIT_REPO}
+fi
+
+(cd ${GECKO_STRINGS} && git pull origin main)
+(cd ${L10N_CENTRAL} && git pull origin main)
+(cd ${MJ_GIT_REPO} && git pull origin master)
+
+${PYTHON_EXE} ${CURDIR}/slackbot.py ${GECKO_STRINGS}/_configs/browser.toml ${MJ_GIT_REPO} ${L10N_CENTRAL}
